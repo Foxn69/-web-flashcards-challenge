7083614d4910bd8e4e9f4cf81c404690cedadbba
IgorKomin <kominigor@gmail.com>
2022-04-22 14:30:30 +0300

    retrieved data from db

--- routes/main.route.js
+++ routes/main.route.js
@@ -2,8 +2,14 @@ const router = require('express').Router();
 const {Themes} = require('../db/models')
 
 
+
 router.route('/')
-  .get((req, res) => {
+  .get(async (req, res)  =>  {
+
+    const dbThemes = await Themes.findAll({raw: true})
+    // console.log(dbThemes)
+    console.log(dbThemes[1].title)
+ 
     res.render('main')
   })
 
